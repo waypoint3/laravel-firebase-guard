e3801655fefb7744b7ef775ecdfaa48f1e998071
Martin Hughes <martin.hughes@easyodds.com>
2024-05-21 11:51:24 +0100

    Starting

---
+++ tests/Unit/Auth/FirebaseGuardTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use LeedsDigital\LaravelFirebaseGuard\Auth\FirebaseGuard;
+
+beforeEach(function () {
+    $this->guard = new FirebaseGuard();
+});
+
+test('checks user is authenticatable', function () {
+    $this->instance
+});
